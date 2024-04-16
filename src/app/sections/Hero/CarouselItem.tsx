6b613d6e4f2ab498e3a5cb9aad2987a7d8c5fbd1
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-04-15 22:42:34 -0300

    feat: created hero carousel item

---
+++ src/app/sections/Hero/CarouselItem.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from 'react';
+import styles from './carouselItem.module.scss';
+
+type Props = {
+  title: string;
+  description: string;
+  location: {
+    city: string;
+    state: string;
+  };
+  url: string;
+  backgroundImage: ReactNode;
+};
+
+export default function CarouselItem({
+  title,
+  description,
+  location,
+  backgroundImage,
+}: Props) {
+  console.log(styles);
+
+  return (
+    <section className={styles['carousel-item']}>
+      <div className={styles['carousel-item__background-image']}>
+        {backgroundImage}
+      </div>
+
+      <div className={styles['carousel-item__container']}>
+        <p className={styles['carousel-item__location']}>
+          {location.city} - {location.state}
+        </p>
+
+        <h1 className={styles['carousel-item__title']}>{title}</h1>
+
+        <h2 className={styles['carousel-item__description']}>{description}</h2>
+      </div>
+    </section>
+  );
+}
