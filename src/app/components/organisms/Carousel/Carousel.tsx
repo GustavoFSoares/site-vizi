fa9929eeacf39e3dc2314fc213eaade631d495a6
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-04-17 16:15:42 -0300

    feat: added features to carousel

--- src/app/components/organisms/Carousel/Carousel.tsx
+++ src/app/components/organisms/Carousel/Carousel.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { useEffect } from 'react';
+
 import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
 
 import styles from './styles/styles.module.scss';
 import NavigationDots from './partials/NavigationDots';
@@ -9,15 +12,27 @@ import NavigationButtons from './partials/NavigationButtons';
 export type Props = {
   navigation?: boolean;
   dotsNavigation?: boolean;
+  disableLoop?: boolean;
   children: JSX.Element | JSX.Element[];
+  startIndex?: number;
+  autoPlay?: boolean;
 };
 
 export default function Carousel({
   children,
   navigation,
   dotsNavigation,
+  disableLoop,
+  startIndex = 0,
+  autoPlay,
 }: Props) {
-  const [emblaRef, emblaApi] = useEmblaCarousel();
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    {
+      loop: !disableLoop,
+      startIndex,
+    },
+    [Autoplay({ playOnInit: autoPlay, delay: 3000 })]
+  );
 
   return (
     <div className={styles['embla']} ref={emblaRef}>
