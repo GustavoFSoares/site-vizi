7323ff78dc193d8c7ed09fea884908e279805b37
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-04-18 11:20:58 -0300

    feat: updated carousel buttons

--- src/app/sections/Hero/index.tsx
+++ src/app/sections/Hero/index.tsx
@@ -12,29 +12,47 @@ import { useBreakpoints } from '@hooks/useBreakpoints';
 export default function Hero() {
   const { isXs } = useBreakpoints();
 
+  const carouselData = [
+    {
+      title: 'Complexo La Bella Toscana',
+      description:
+        'Complexo de condomínios com mais de 10 espaços de lazer. Lotes a partir de 128m² em uma excelente localização na região metropolitana de Curitiba',
+      location: { city: 'São José dos Pinhais', state: 'PR' },
+      url: '/#cta/',
+      backgroundImage: (
+        <Image
+          className='h-full'
+          src={BackgroundImage}
+          alt={'Complexo La Bella Toscana'}
+        />
+      ),
+    },
+    {
+      title: 'Complexo 2',
+      description:
+        'Complexo 2. Lotes a partir de 128m² em uma excelente localização na região metropolitana de Curitiba',
+      location: { city: 'São José dos Pinhais', state: 'PR' },
+      url: '/#cta2/',
+      backgroundImage: (
+        <Image className='h-full' src={BackgroundImage} alt={'Complexo 2'} />
+      ),
+    },
+  ];
+
   return (
     <div>
       <Carousel navigation dotsNavigation={!isXs}>
-        <Carousel.Slide>
-          <CarouselItem
-            title='Complexo La Bella Toscana'
-            description='Complexo de condomínios com mais de 10 espaços de lazer. Lotes a partir de 128m² em uma excelente localização na região metropolitana de Curitiba'
-            location={{ city: 'São José dos Pinhais', state: 'PR' }}
-            url='/#cta/'
-            backgroundImage={
-              <Image src={BackgroundImage} alt={'Complexo La Bella Toscana'} />
-            }
-          />
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <CarouselItem
-            title='Complexo 2'
-            description='Complexo 2. Lotes a partir de 128m² em uma excelente localização na região metropolitana de Curitiba'
-            location={{ city: 'São José dos Pinhais', state: 'PR' }}
-            url='/#cta2/'
-            backgroundImage={<Image src={BackgroundImage} alt={'Complexo 2'} />}
-          />
-        </Carousel.Slide>
+        {carouselData.map((item, itemIndex) => (
+          <Carousel.Slide key={itemIndex}>
+            <CarouselItem
+              title={item.title}
+              description={item.description}
+              location={item.location}
+              url={item.url}
+              backgroundImage={item.backgroundImage}
+            />
+          </Carousel.Slide>
+        ))}
       </Carousel>
     </div>
   );
