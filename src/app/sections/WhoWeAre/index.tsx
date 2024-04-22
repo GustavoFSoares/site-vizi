5d5397c2a696ed944d7f818f851ce0dfd1ca93a7
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-04-22 16:32:34 -0300

    feat: created delivered list

--- src/app/sections/WhoWeAre/index.tsx
+++ src/app/sections/WhoWeAre/index.tsx
@@ -4,31 +4,38 @@ import SectionSubtitle from '@atoms/SectionSubtitle';
 import ShortSectionTitle from '@atoms/ShortSectionTitle';
 
 import style from './whoWeAre.module.scss';
+import DeliveredList from './DeliveredList';
 
 export default function WhoWeAre() {
   return (
-    <PageSection className={style['who-we-are']}>
-      <div className={style['who-we-are__header']}>
-        <ShortSectionTitle>Quem somos</ShortSectionTitle>
-        <SectionSubtitle>
-          Um ecossistema completo para viabilizar a construção de sonhos
-        </SectionSubtitle>
-      </div>
+    <>
+      <PageSection className={style['who-we-are']}>
+        <div className={style['who-we-are__header']}>
+          <ShortSectionTitle>Quem somos</ShortSectionTitle>
+          <SectionSubtitle>
+            Um ecossistema completo para viabilizar a construção de sonhos
+          </SectionSubtitle>
+        </div>
 
-      <div className={style['who-we-are__content']}>
-        <p>
-          Na Vizi Lotes, simplificamos o caminho para que você alcance seu
-          desejo de possuir um terreno próprio.
-        </p>
+        <div className={style['who-we-are__content']}>
+          <p>
+            Na Vizi Lotes, simplificamos o caminho para que você alcance seu
+            desejo de possuir um terreno próprio.
+          </p>
 
-        <p>
-          Especialistas no assunto, firmamos parcerias com as mais renomadas
-          loteadoras do Brasil, realizando uma curadoria das principais
-          oportunidades e garantindo os melhores lotes para você.
-        </p>
+          <p>
+            Especialistas no assunto, firmamos parcerias com as mais renomadas
+            loteadoras do Brasil, realizando uma curadoria das principais
+            oportunidades e garantindo os melhores lotes para você.
+          </p>
 
-        <Button className={style['who-we-are__cta']}>Saiba mais</Button>
-      </div>
-    </PageSection>
+          <Button className={style['who-we-are__cta']}>Saiba mais</Button>
+        </div>
+      </PageSection>
+
+      <PageSection className={style['who-we-are__delivered']}>
+        <DeliveredList className={style['who-we-are__delivered-content']} />
+      </PageSection>
+    </>
   );
 }
