f24d2c39769b1c76de3adba83d1b5546dced9a7d
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-04-22 16:19:51 -0300

    feat: added padding options to button

--- src/app/components/atoms/Button/index.tsx
+++ src/app/components/atoms/Button/index.tsx
@@ -5,11 +5,20 @@ import style from './button.module.scss';
 type Props = {
   children: string;
   className?: string;
+  px?: string;
+  py?: string;
 };
 
-export default function Button({ children, className }: Props) {
+export default function Button({ children, className, px, py }: Props) {
   return (
-    <button className={bindClass(style['button'], className)}>
+    <button
+      className={bindClass(
+        style['button'],
+        className,
+        px && `px-${px}`,
+        py && `py-${py}`
+      )}
+    >
       {children}
     </button>
   );
