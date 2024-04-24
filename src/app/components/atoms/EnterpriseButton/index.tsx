193899a1781ccdf00df7e3eefb3640798d776249
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-04-24 15:20:22 -0300

    fear: added uppercase state to button

--- src/app/components/atoms/EnterpriseButton/index.tsx
+++ src/app/components/atoms/EnterpriseButton/index.tsx
@@ -6,19 +6,22 @@ import style from './enterpriseButton.module.scss';
 type Props = {
   children: String | ReactNode;
   selected?: boolean;
+  uppercase?: boolean;
   onClick?: () => void;
 };
 
 export default function EnterpriseButton({
   children,
   selected,
+  uppercase,
   onClick,
 }: Props) {
   return (
     <button
       className={bindClass(
         style['enterprise-button'],
-        selected && style['enterprise-button--selected']
+        selected && style['enterprise-button--selected'],
+        uppercase && style['enterprise-button--uppercase']
       )}
       onClick={onClick}
     >
