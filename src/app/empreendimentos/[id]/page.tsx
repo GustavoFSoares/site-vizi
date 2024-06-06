ee2e490e32a8967745cef09f9d8ae857b807ae97
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-06-06 15:59:00 -0300

    feat: getting data from api

--- src/app/empreendimentos/[id]/page.tsx
+++ src/app/empreendimentos/[id]/page.tsx
@@ -1,10 +1,30 @@
-import EnterprisesList from './sections/EmptySection';
+import getHost from '@helpers/getHost';
 
-export default function EnterprisesById() {
+import { IEnterpise } from '@interfaces/EnterpriseInterface';
+
+import Header from './sections/Header';
+import HeroInfrastructure from './sections/Hero&Infrastructure';
+
+import EmptySection from './sections/EmptySection';
+
+export default async function EnterprisesById() {
+  const { api } = getHost();
+
+  async function getEnterpriseData(): Promise<IEnterpise> {
+    const data = await fetch(`${api}/enterprises/1`);
+
+    return await data.json();
+  }
+
+  const enterpriseData = await getEnterpriseData();
+
   return (
     <>
-      <h1>12354</h1>
-      <EnterprisesList />
+      <Header businessName={enterpriseData.title} />
+
+      <HeroInfrastructure infrastructure={enterpriseData.infrastructure} />
+
+      <EmptySection />
     </>
   );
 }
